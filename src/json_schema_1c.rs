c779080b97a969355e38b35b43e7871a3c34774b
Toveal <mikaseet12@gmail.com>
2024-11-19 19:30:45 +0500

    Revert "Изменены шаблоны в поле формат"
    This reverts commit 72e3140825b7109d63608a8ca3ca41a4ecf5a402.

--- src/json_schema_1c.rs
+++ src/json_schema_1c.rs
@@ -338,10 +338,10 @@ impl JsonSchema1C {
                 err_it
                     .map(|e| {
                         format
-                            .replace("{{$path}}", &e.instance_path.to_string())
-                            .replace("{{$instance}}", &e.instance.to_string())
-                            .replace("{{$schema_path}}", &e.schema_path.to_string())
-                            .replace("{{$error}}", &e.to_string())
+                            .replace("{path}", &e.instance_path.to_string())
+                            .replace("{instance}", &e.instance.to_string())
+                            .replace("{schema_path}", &e.schema_path.to_string())
+                            .replace("{error}", &e.to_string())
                     })
                     .collect()
             } else {
