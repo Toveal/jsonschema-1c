e9728ac5bbcad38d017c190bcbdb396ca7217c71
Toveal <mikaseet12@gmail.com>
2024-11-22 20:08:19 +0500

    Добавлены вспомогательные функции

--- src/tools.rs
+++ src/tools.rs
@@ -8,3 +8,13 @@ pub fn unpack_first_param(params: Option<&mut [Variant]>) -> Result<&Variant, Js
         .first()
         .ok_or(JsonSchema1CError::ParamUnpackError)
 }
+
+pub fn unpack_two_params(
+    params: Option<&mut [Variant]>,
+) -> Result<[&mut Variant; 2], JsonSchema1CError> {
+    if let Some([p1, p2]) = params {
+        Ok([p1, p2])
+    } else {
+        Err(JsonSchema1CError::ParamUnpackError)
+    }
+}
